34a34f74ad4d9d1ac3eb4deb184a0b7dfc8afa64
omegaburner <snake_x@web.de>
2016-07-27 17:50:12 +0200

    Update __init__.py

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,5 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-    'GOOGLEMAPS_KEY': 'AIzaSyBsI4bqRiVHIzz4rLVqjD_tr6r6AFnIt00'
+    'GOOGLEMAPS_KEY': ''
 }
